8c52e3af071d699992ef367754fbd9e8795eaef9
Jiyun_Jung <jammy9087@gmail.com>
2024-07-05 20:32:00 +0900

    Server 구축3

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,4 +2,9 @@
 
 from django.db import models
 
-class It
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
