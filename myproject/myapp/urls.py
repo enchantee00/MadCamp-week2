6c4906ee1deee4b71445e6e8ef42ad456594874f
Jiyun_Jung <jammy9087@gmail.com>
2024-07-05 15:41:03 +0900

    Server 구축

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,11 @@
+# myapp/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
+
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
