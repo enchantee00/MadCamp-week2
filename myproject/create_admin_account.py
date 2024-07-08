bb3307cffaea64e81cca3d61d33cefebbf169693
root <jammy9087@gmail.com>
2024-07-08 15:13:29 +0000

    Admin page 세부사항 구현

--- myproject/create_admin_account.py
+++ myproject/create_admin_account.py
@@ -2,6 +2,7 @@ import os
 import django
 from django.utils import timezone
 from django.contrib.auth.hashers import make_password
+from datetime import timedelta
 
 # Django 프로젝트의 설정 모듈 설정
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
@@ -10,25 +11,25 @@ django.setup()
 from myapp.models import User
 
 # 원하는 비밀번호
-password = 'youwillneverthinkthisispassword'
+password = 'password'
 
 # 비밀번호 해시 생성
 hashed_password = make_password(password)
 
 # 관리자 계정 생성
 admin_user = User.objects.create(
-    username='yigyeom',
+    username='daemo',
     password=hashed_password,
-    role='admin',
-    play_count=-1,
-    best_score=-1,
-    point=-1,
-    item_count=-1,
-    item_slow_down=-1,
-    item_no_bomb=-1,
-    item_big_size=-1,
-    item_triple_points=-1,
-    total_duration=timezone.now(),
+    role='player',
+    play_count=12,
+    best_score=1385,
+    point=5000,
+    item_count=12,
+    item_slow_down=3,
+    item_no_bomb=3,
+    item_big_size=3,
+    item_triple_points=3,
+    total_duration= timedelta(20000),
     created_at=timezone.now()
 )
 
--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,17 +1,25 @@
 # myapp/urls.py
 from django.urls import path, include
 # from rest_framework.routers import DefaultRouter
-from .views import LoginView, UserListView, GameItemClickView, GameStartView, GameEndView, GameItemBuyView
+from .views import LoginView, UserListView, GameItemClickView, GameStartView, GameEndView, GameItemBuyView, Item1PressedView, Item2PressedView, Item3PressedView, Item4PressedView, AppLoginView, QueryView
 
 # router = DefaultRouter()
 # router.register(r'items', ItemViewSet)
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name='login'),
-    path('users', UserListView.as_view(), name='user-list'),
-    # path('app/login/', AppLoginView.as_view(), name='app-login')
+    #app
+    path('app/login', AppLoginView.as_view(), name='app-login'),
     path('app/turn/item/use', GameItemClickView.as_view(), name='turn-item-click'),
     path('app/turn/start', GameStartView.as_view(), name='turn-start'),
     path('app/turn/end', GameEndView.as_view(), name='turn-end'),
-    path('app/item/buy', GameItemBuyView.as_view(), name='item-buy')
+    path('app/item/buy', GameItemBuyView.as_view(), name='item-buy'),
+    
+    #web
+    path('login', LoginView.as_view(), name='login'),
+    path('item1/pressed', Item1PressedView.as_view(), name='item1-pressed'),
+    path('item2/pressed', Item2PressedView.as_view(), name='item2-pressed'),
+    path('item3/pressed', Item3PressedView.as_view(), name='item3-pressed'),
+    path('item4/pressed', Item4PressedView.as_view(), name='item4-pressed'),
+    path('users', UserListView.as_view(), name='user-list'),
+    path('query', QueryView().as_view(), name='query-freely')
 ]
